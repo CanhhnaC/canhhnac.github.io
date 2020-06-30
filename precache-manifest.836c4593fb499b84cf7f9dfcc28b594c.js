7165ae1aed95440bac224e53836793d247979227
CanhhnaC <nguyentrancanh81@gmail.com>
2020-07-01 00:04:38 +0700

    add feature
    add theme

---
+++ precache-manifest.836c4593fb499b84cf7f9dfcc28b594c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564bcb328d8f2c4068ef407d3646eaa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b86d9001c1fee4cb987d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7add0b8b15ff2ee4e4f0",
+    "url": "/static/js/2.80d03ccf.chunk.js"
+  },
+  {
+    "revision": "f07796f4f30300629a9fa688e41af979",
+    "url": "/static/js/2.80d03ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86d9001c1fee4cb987d",
+    "url": "/static/js/main.12c36372.chunk.js"
+  },
+  {
+    "revision": "c3727f7525fdd103b241",
+    "url": "/static/js/runtime-main.57f6e5ab.js"
+  }
+]);
